bd35e4f952b32d5ebe281f9d90a16b2d259a7421
praveensingh1999 <praveen969500@gmail.com>
2024-02-16 17:42:49 +0530

    added alert fxnality for enable mode and light mode.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,33 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import Alert from './components/Alert';
 
 function App() {
   const [mode, setMode] = useState('dark');  // wheather dark mode is enabled or not
-  
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+setAlert(null);
+    },1500);
+  }
+
   const toggleMode = () => {
     if(mode === 'light')
       { setMode('dark')
       document.body.style.backgroundColor = "#B4B4B8";
+      showAlert("dark mode has been enabled", "success");
     }
     else
     { setMode('light')
     document.body.style.backgroundColor = "#E0F4FF"
+    showAlert("light mode has been enabled", "success");
   }
 
   }
@@ -23,6 +37,7 @@ function App() {
   return (
     <>
       <Navbar title = "TextUtility"  about_text = "About Us"  mode = {mode}  toggleMode={toggleMode}/>
+      <Alert alert = {alert} />
       <div className='container'>
       <TextForm heading = "Text Analyser " mode = {mode}/>
       </div>
